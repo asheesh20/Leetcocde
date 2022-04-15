ab4223aca93a6b682bb00d3c53ca697b33247eb0
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-04-15 22:13:32 +0530

    Trim a Binary Search Tree

---
+++ trimabinarysearchtree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* R, int L, int H) {
+         if (!R) return R;
+        if (R->val < L) return trimBST(R->right,L,H);
+        else if (R->val > H) return trimBST(R->left,L,H);
+        R->left = trimBST(R->left,L,H) ;
+        R->right = trimBST(R->right,L,H);
+        return R;
+    }
+};
