7b7810c45bfcf580aed24403d2d0feb7f2b15ff5
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-04-18 18:11:34 +0530

    Kth Smallest Element in a BST

---
+++ kthsmallestelementbst.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int ans;
+    int kthSmallest(TreeNode* root, int &k) {
+        if(root){
+            kthSmallest(root->left,k);
+            k--;
+            if(k==0) ans=root->val;
+            kthSmallest(root->right,k);
+        }
+        return ans;
+    }
+};
