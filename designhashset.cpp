c8b93508ea4a729bdcb6edf8bcec59e9bed133ec
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-04-21 05:59:06 +0530

    Design HashSet

---
+++ designhashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+    int size_ = 101;
+    vector<list<int>> set_;
+public:
+    MyHashSet() : set_(size_, list<int>()){
+        
+    }
+    
+    bool search(int key) {
+        auto& l = set_[key % size_];
+        auto it = find(l.begin(), l.end(), key);
+        return it != l.end();
+    }
+    
+    void add(int key) {
+        if (!search(key)) {
+            set_[key % size_].emplace_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        if (search(key)) {
+            set_[key % size_].remove(key);
+        }
+    }
+    
+    bool contains(int key) {
+        return search(key);
+    }
+};
