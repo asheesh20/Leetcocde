a4c5c2448dd217f19a057ac6f9fbbbd36bcf4315
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-05-07 00:19:26 +0530

    SUGARCANE

---
+++ SUGARCANE.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	ll t,n;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		cout<<((n*50)-(0.2*n*50+0.2*n*50+0.3*n*50))<<endl;
+	}
+	return 0;
+}
