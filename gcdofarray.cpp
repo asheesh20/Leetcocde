026b51adc135705657248f0d61c8e6212893a040
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-05-06 23:17:42 +0530

    1979. Find Greatest Common Divisor of Array

---
+++ gcdofarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    int findGCD(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        int ans=gcd(nums[0],nums[nums.size()-1]);
+        
+        return ans;
+    }
+};
