364ba033a82901f73a90858964fa02806c08e681
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-05-15 03:14:48 +0530

    ASCII Value of a Character

---
+++ ASCIIofcharacter.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define ull unsigned long long 
+using namespace std;
+int32_t main(){
+	char ch;
+	cin>>ch;
+	cout<<"ASCII Value is "<<int(ch)<<endl;
+	return 0;
+}
