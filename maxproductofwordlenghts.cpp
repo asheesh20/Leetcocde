3c8acff120471c9e98d98ed4228795e1e3d01c5e
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-05-29 06:33:48 +0530

    318. Maximum Product of Word Lengths

---
+++ maxproductofwordlenghts.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	int maxProduct(vector<string>& words) {
+		vector<int> bitMasks;
+		for(auto& str : words) { 
+			int bitMask = 0;
+			for(auto& c : str) 
+				bitMask |= 1 << (c - 'a');
+			bitMasks.push_back(bitMask);
+		}
+
+		int ans = 0;
+		for(int i = 0; i < words.size(); i++) { 
+			for(int j = i + 1; j < words.size(); j++) {
+				if( (bitMasks[i] & bitMasks[j]) > 0 )
+					continue;
+
+				int product = words[i].size() * words[j].size();
+				ans = max(ans, product);
+			}
+		}
+		return ans;
+	}
+};
