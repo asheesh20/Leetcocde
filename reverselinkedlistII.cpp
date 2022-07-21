b27f37e3a7b50f5d5da6c52e61a3d433e37f6c4a
Asheesh Kumar <asheesh220720@gmail.com>
2022-07-21 05:50:03 +0530

    92. Reverse Linked List II

---
+++ reverselinkedlistII.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(left==right)return head;
+        ListNode* curr=head;
+        ListNode*rest=head;
+        ListNode*next;
+        ListNode*ptr;
+        ListNode*temp;
+        int count=1;
+        while(count<right){
+            if(count<left){
+                temp=curr;
+                curr=curr->next;
+            }
+            rest=rest->next;
+            count++;
+        }
+        if(rest->next==NULL)ptr=NULL;
+        else ptr=rest->next;
+        
+        while(left<=right){
+            next=curr->next;
+            curr->next=ptr;
+            ptr=curr;
+            curr=next;
+            left++;
+        }
+        if(!temp)return ptr;
+        temp->next=ptr;
+        return head;
+    }
+};
