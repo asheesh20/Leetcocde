286e22c3b63fae4969fa42a02e8aa38aae0dfc8f
Asheesh Kumar <asheesh220720@gmail.com>
2022-07-30 08:56:43 +0530

    231. Power of Two

--- poweroftwo.cpp
+++ poweroftwo.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
@@ -8,5 +10,22 @@ public:
             else return false;
         }
         return true;
+    }
+};
+
+2nd Approach :
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int c=0;
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n!=0){
+            if(n&1) c++;
+            n=n>>1;
+        }
+        if(c==1) return true;
+        return false;
     }
 };
