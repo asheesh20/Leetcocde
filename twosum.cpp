f5855eeaca5d060fc99a634c3e718848b6ff83ee
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-03 12:41:49 +0530

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -14,5 +16,21 @@ public:
             
         }
         return {-1, -1};
+    }
+};
+
+2nd Approach :
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target) {
+                    return {i,j};
+                }
+            }
+        }
+        return {0};
     }
 };
