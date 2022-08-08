f9d7aac2c38b2f44c4e30689b48eaa5236041242
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-09 00:44:21 +0530

    88. Merge Sorted Array

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -16,5 +18,19 @@ public:
         
         while(j>=0)
             nums1[k--] = nums2[j--];
+    }
+};
+
+2nd Approach (Easy):
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int j=m;
+        for(int i=0;i<n;i++){
+            nums1[j]=nums2[i];
+            j++;
+        }
+        sort(nums1.begin(),nums1.end());
     }
 };
