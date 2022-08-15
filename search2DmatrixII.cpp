d58c092e4e35d423562f5d919bab8e542f21c29f
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-15 19:27:58 +0530

    240. Search a 2D Matrix II

--- search2DmatrixII.cpp
+++ search2DmatrixII.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
@@ -18,5 +20,23 @@ public:
         }
         return false;
         
+    }
+};
+
+2nd Approach (Easy) :
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int i=0,j=col-1;
+        while(i<row && j>=0){
+            int element=matrix[i][j];
+            if(element==target) return 1;
+            else if(element>target) j--;
+            else i++;
+        }
+        return 0;
     }
 };
