32b7d2f9be3e59ec97196cfc7db002a1dbceda33
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-15 17:46:28 +0530

    74. Search a 2D Matrix

--- search2dmatrix.cpp
+++ search2dmatrix.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
@@ -13,5 +15,26 @@ public:
                 j++;
         }
         return false;
+    }
+};
+
+2nd Approach (Easy):
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size(); //according to question
+        int col=matrix[0].size(); // according to question
+        int start=0;
+        int end=row*col-1;
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            int element=matrix[mid/col][mid%col];// row , col 
+            if(element==target) return 1;
+            else if(element<target) start=mid+1;
+            else end=mid-1;
+            mid=start+(end-start)/2;
+        }
+        return 0;
     }
 };
