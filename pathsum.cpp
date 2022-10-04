93fc30a1638b585f9114f14cb6fb28b3620fd2b8
Asheesh Kumar <asheesh220720@gmail.com>
2022-10-04 14:21:53 +0530

    112. Path Sum

--- pathsum.cpp
+++ pathsum.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        
-        if(!root) return false;
-        
-        if(root->left == 0 && root->right == 0) return targetSum == root->val;
-        
-        return (hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val));
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)return false;
+        if(!root->left&&!root->right)return sum==root->val;
+        sum-=root->val;
+        return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);
     }
 };
