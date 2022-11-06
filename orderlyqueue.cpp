d73671a826baa7812d49b95c70a39411ed774f5b
Asheesh Kumar <asheesh220720@gmail.com>
2022-11-06 07:30:42 +0530

    899. Orderly Queue

---
+++ orderlyqueue.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+   static string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            s.reserve(size(s) * 2);
+            string_view ans{s};
+            s += s;
+            string_view sv{s};
+            for (int i = 1; i < size(ans); ++i)
+                ans = min(ans, sv.substr(i, size(ans)));
+            return string{ans};
+        }
+        sort(begin(s), end(s));
+        return s;
+    }
+
+};
