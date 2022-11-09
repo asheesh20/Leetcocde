3e36c575896fb0cf309bd748f7f1529c82a04b56
Asheesh Kumar <asheesh220720@gmail.com>
2022-11-09 11:37:57 +0530

    901. Online Stock Span

---
+++ onlinestockspanc.cpp
@@ -0,0 +1,20 @@
+class StockSpanner {
+public:
+   stack<pair<int,int>> s;
+    StockSpanner() {        
+    }
+    
+    int next(int price) {
+       int res = 1;
+       if(!s.size())s.push({price,1});  
+       else{
+         while(s.size() && s.top().first<=price){
+             res+= s.top().second;
+             s.pop();
+         }
+         s.push({price,res});
+       }
+
+       return res;
+    }
+};
