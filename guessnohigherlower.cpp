e17c7ed6b913d6e533e38d73215325628594d1f5
Asheesh Kumar <asheesh220720@gmail.com>
2022-11-16 11:26:59 +0530

    374. Guess Number Higher or Lower

---
+++ guessnohigherlower.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            int val = guess(mid);
+            if(val == 0) return mid;
+            else if(val == -1 ) right = mid-1;
+            else left = mid+1;
+        }
+        return -1;
+    }
+};
