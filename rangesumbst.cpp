1cb59ea2d688a45b0eff7bc9a3cdb8f1875565c6
Asheesh Kumar <asheesh220720@gmail.com>
2022-12-07 13:42:11 +0530

    938. Range Sum of BST

---
+++ rangesumbst.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+int rangeSumBST(TreeNode* root, int L, int R) {
+  if (root == nullptr) return 0;
+  return (root->val >= L && root->val <= R ? root->val : 0) +
+    rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
+ }
+};
