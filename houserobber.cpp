a42576e99d4838c7e92643edfef0b033595f3a07
Asheesh Kumar <asheesh220720@gmail.com>
2022-12-14 18:15:37 +0530

    198. House Robber

--- houserobber.cpp
+++ houserobber.cpp
@@ -5,11 +5,9 @@ public:
         int dp[n+1];    
         dp[0] = 0;    
         dp[1] = nums[0];  
-        
         for (int i{2};i<n+1;i++){
             dp[i] = max(nums[i-1]+dp[i-2] , dp[i-1]);
         }
-        
         return dp[n];
     }
 };
