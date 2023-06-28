a16fd5a1c8218da44b53684cb7cc78cf3833b472
Asheesh Kumar <asheesh220720@gmail.com>
2023-06-28 18:17:20 +0530

    poweroftwo.cpp

--- poweroftwo.cpp
+++ poweroftwo.cpp
@@ -1,31 +1,42 @@
-1st Approach :
+//1st Approach : Basic Divison Approach 
 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-     if(n==0||n<0) return false;
-     if(n==1) return true;
-        while(n>1){
-            if(n%2==0) n/=2;
-            else return false;
+        if(n<=0) return 0;
+        while(n%2==0) {
+            n/=2;
         }
-        return true;
+        if(n==1) return 1;
+        return 0;
     }
 };
 
-2nd Approach :
+//2nd Approach : count set bits
 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int c=0;
-        if(n<=0) return false;
-        if(n==1) return true;
+        if(n<=0) return 0;
+        int c(0);
         while(n!=0){
             if(n&1) c++;
             n=n>>1;
         }
-        if(c==1) return true;
-        return false;
+        if(c==1) return 1;
+        return 0;
+        
+    }
+};
+
+//3rd Approach : Bitwise operation 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return 0;
+        if((n&(n-1))==0) return 1;
+        return 0;
+        
     }
 };
