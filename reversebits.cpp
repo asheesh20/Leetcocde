c1f06829b0a26d06e068ca96d0566ce75b7df2f2
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-02 23:22:49 +0530

    reversebits.cpp

--- reversebits.cpp
+++ reversebits.cpp
@@ -1,3 +1,18 @@
+//1st Approach 
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int mask=0;
+        for(int i=0;i<32;i++){
+            int bit=n&1;
+            mask=(mask<<1)|bit;
+            n=n>>1;
+        }
+        return mask;
+    }
+};
+
+// 2nd Approach 
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
