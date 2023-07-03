c8939cfe05428ca097f4c051a41b196fbaa8da26
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-03 10:34:36 +0530

    allduplicatesarray.cpp

--- allduplicatesarray.cpp
+++ allduplicatesarray.cpp
@@ -3,9 +3,11 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> v;
         for(int i=0;i<nums.size();i++){
-            if(nums[abs(nums[i])-1]>0) nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
-            else v.push_back(abs(nums[i]));
+            int index=abs(nums[i])-1;
+            if(nums[index]<0) v.emplace_back(abs(nums[i]));
+            nums[index]=-nums[index];
         }
         return v;
+        
     }
 };
