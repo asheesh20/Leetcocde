0f9adef35b024f23fd0f7ef246fded9f115385eb
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-06 12:57:16 +0530

    sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,3 +1,5 @@
+// Brute Force 
+// TC: O(sqrt(n))
 class Solution {
 public:
     int mySqrt(int x) {
@@ -8,6 +10,28 @@ public:
             if(i<=x/i){
                 ans=i;
             }
+        }
+        return ans;
+    }
+};
+
+// Binary Search
+// TC : O(logN)
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int s=0,e=x;
+        long long int m=s+(e-s)/2;
+        long long int ans;;
+        while(s<=e){
+            if(m*m==x) return m;
+            else if(m*m>x) e=m-1;
+            else {
+                ans=m;
+                s=m+1;
+            }
+            m=s+(e-s)/2;
         }
         return ans;
     }
